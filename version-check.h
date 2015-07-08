6763fb4d16506b39c8ddca598fdb0f0a1fd506ab
Rob Hoelz <rob@hoelz.ro>
2015-07-07 20:22:36 -0500

    Be more exact in my version check

--- version-check.h
+++ version-check.h
@@ -1,5 +1,5 @@
 #include <xapian/version.h>
 
-#if XAPIAN_MAJOR_VERSION != 1 || XAPIAN_MINOR_VERSION != 2
-#  error "Invalid Xapian version; expected 1.2.*"
+#if XAPIAN_MAJOR_VERSION != 1 || XAPIAN_MINOR_VERSION != 2 || XAPIAN_REVISION < 13
+#  error "Invalid Xapian version; expected >= 1.2.13"
 #endif
